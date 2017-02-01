385d2473059bf6d26cb556b067ab82643e0c813a
Rija Aamir <aamir.rija@gmail.com>
2017-02-01 02:43:29 +0000

    Static Toy App

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :microposts
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+  resources :microposts 
   resources :users
-  root 'users#index'
+  root 'static_pages#home'
 end
